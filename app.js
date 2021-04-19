fe664114afd6a0027e904ea2b3de55544b35aa32
Kevin <kevinsebastianmathews@gmail.com>
2021-04-19 00:23:18 -0700

    usermodel

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 
+const express = require("express");
+const app = express();
+
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
 
-const express = require("express");
-const app = express();
 
-app.get("/", (req, res) => res.send("Wassup"));
+
+app.get("/", (req, res) => {
+  
+    const user=new User({
+        handle: 'Jim',
+        email: 'jim@jim.jim',
+        password: 'jimisgreat123'
+    })
+    user.save()
+      res.send("Wassup");
+});
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
+
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
